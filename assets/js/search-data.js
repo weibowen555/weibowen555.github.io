1fb836ee795bf33198f0edea39a0df9f4aa4e883
weibowen555 <31663419+weibowen555@users.noreply.github.com>
2025-11-10 02:46:27 +0000

    Deploying to gh-pages from @ weibowen555/weibowen555.github.io@b9bed125aed857cd0df231f18df4e800f587d0a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-about",
+          title: "about",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
