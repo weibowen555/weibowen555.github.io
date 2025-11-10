2a9dc37f655f46dfa07122a9254aed783ce258f8
weibowen555 <31663419+weibowen555@users.noreply.github.com>
2025-11-10 02:32:12 +0000

    Deploying to gh-pages from @ weibowen555/weibowen555.github.io@a3234f3938ec3992e29aa5610598a061d72d549f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%77%65%69%32@%67%6D%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
