d5fd9138471a8338035909ab609c0e93acc190ac
weibowen555 <31663419+weibowen555@users.noreply.github.com>
2025-11-10 03:43:00 +0000

    Deploying to gh-pages from @ weibowen555/weibowen555.github.io@47d640a1a93095733d1012af9258a3e76625699c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,16 +447,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-making-sense-of-llm-decisions-a-prototype-based-framework-for-explainable-classification-has-been-accepted-for-an-oral-presentation-at-aaai-2026",
+          title: '🎉 Our paper “Making Sense of LLM Decisions: A Prototype-based Framework for Explainable...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
